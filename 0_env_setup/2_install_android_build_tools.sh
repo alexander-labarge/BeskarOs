cd8f6b1e2d340731a6aa1c640be19d7b7caaf7c9
Alexander La Barge <alex@labarge.dev>
2024-07-30 07:25:19 -0400

    automated 22.04 package install

--- 0_env_setup/2_install_android_build_tools.sh
+++ 0_env_setup/2_install_android_build_tools.sh
@@ -3,7 +3,14 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Check if dependencies are already installed before installing
+# Variables and paths
+REPO_URL="https://storage.googleapis.com/git-repo-downloads/repo"
+REPO_KEY="8BB9AD793E8E6153AF0F9A4416530D5E920F5C65"
+REPO_TEMP=$(mktemp /tmp/repo.build.XXXXXXXXXX.$(date +%Y%m%d%H%M%S))
+PACKAGES_FILE="ubuntu_22.04_packages_required.txt"
+FAILED_PACKAGES=()
+
+# Functions
 check_dependency() {
     dpkg -s "$1" &> /dev/null
 }
@@ -14,89 +21,83 @@ install_dependency() {
     fi
 }
 
-# Check existence before removing
 remove_if_exists() {
     if [ -e "$1" ]; then
         sudo rm "$1"
     fi
 }
 
-# List of packages to install
-packages=(
-  "git-core"
-  "gnupg"
-  "flex"
-  "bison"
-  "build-essential"
-  "zip"
-  "vim"
-  "curl"
-  "zlib1g-dev"
-  "wget"
-  "libc6-dev-i386"
-  "x11proto-core-dev"
-  "libx11-dev"
-  "lib32z1-dev"
-  "libgl1-mesa-dev"
-  "libxml2-utils"
-  "xsltproc"
-  "unzip"
-  "fontconfig"
-)
+install_packages() {
+    while IFS= read -r package; do
+        echo "Installing $package..."
+        if install_dependency "$package"; then
+            echo "$package installed successfully."
+        else
+            echo "Failed to install $package."
+            FAILED_PACKAGES+=("$package")
+        fi
+        echo
+    done < "$PACKAGES_FILE"
 
-# Array to hold the names of packages that failed to install
-failed_packages=()
+    if [ ${#FAILED_PACKAGES[@]} -ne 0 ]; then
+        echo "The following packages failed to install:"
+        for pkg in "${FAILED_PACKAGES[@]}"; do
+            echo "- $pkg"
+        done
+    else
+        echo "All packages installed successfully."
+    fi
+}
 
-# Loop through each package and attempt to install
-for package in "${packages[@]}"; do
-  echo "Installing $package..."
-  if install_dependency "$package"; then
-    echo "$package installed successfully."
-  else
-    echo "Failed to install $package."
-    failed_packages+=("$package")
-  fi
-  echo
-done
+remove_existing_repo() {
+    if command -v repo > /dev/null; then
+        echo "Removing existing repo tool..."
+        sudo rm -f "$(command -v repo)"
+    fi
+}
 
-# Print packages that failed to install
-if [ ${#failed_packages[@]} -ne 0 ]; then
-  echo "The following packages failed to install:"
-  for pkg in "${failed_packages[@]}"; do
-    echo "- $pkg"
-  done
-else
-  echo "All packages installed successfully."
-fi
+install_repo_tool() {
+    remove_existing_repo
+    curl -o "${REPO_TEMP}" "${REPO_URL}"
+    gpg --recv-keys "${REPO_KEY}"
+    if curl -s "${REPO_URL}.asc" | gpg --verify - "${REPO_TEMP}"; then
+        sudo install -m 755 "${REPO_TEMP}" /usr/local/bin/repo
+        repo --version
+    else
+        echo "GPG verification failed. Repo tool installation aborted."
+        exit 1
+    fi
+    rm -r "${REPO_TEMP}"
+}
 
-# Install Repo tool
-repo_temp=$(mktemp /tmp/repo.XXXXXXXXX)
-curl -o "${repo_temp}" https://storage.googleapis.com/git-repo-downloads/repo
-gpg --recv-keys 8BB9AD793E8E6153AF0F9A4416530D5E920F5C65
-if curl -s https://storage.googleapis.com/git-repo-downloads/repo.asc | gpg --verify - "${repo_temp}"; then
-    sudo install -m 755 "${repo_temp}" /usr/local/bin/repo
-    repo --version
-else
-    echo "GPG verification failed. Repo tool installation aborted."
-    exit 1
-fi
-rm -r "${repo_temp}"
+configure_git() {
+    read -p "Enter your Git user name: " git_username
+    read -p "Enter your Git user email: " git_useremail
 
-# Prompt user for Git configuration
-read -p "Enter your Git user name: " git_username
-read -p "Enter your Git user email: " git_useremail
+    git config --global user.name "$git_username"
+    git config --global user.email "$git_useremail"
 
-# Configure Git user name and email
-git config --global user.name "$git_username"
-git config --global user.email "$git_useremail"
+    echo "Git configuration set:"
+    git config --global --get user.name
+    git config --global --get user.email
+}
 
-echo "Git configuration set:"
-git config --global --get user.name
-git config --global --get user.email
+verify_repo_installation() {
+    if command -v repo > /dev/null; then
+        echo "Repo tool is available on PATH."
+    else
+        echo "Failed to install Repo tool."
+    fi
+}
 
-# Verify repo installation
-if command -v repo > /dev/null; then
-    echo "Repo tool installed successfully and is available on PATH."
-else
-    echo "Failed to install Repo tool."
-fi
+# script execution
+install_packages
+install_repo_tool
+configure_git
+verify_repo_installation
+
+echo "#########################################################################"
+echo "Ubuntu 22.04 Updated & Required Packages Installed"
+echo "Repo tool installed successfully and is available on PATH."
+echo "Git configuration set."
+echo "#########################################################################"
